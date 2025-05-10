c959eeb80f37302eacf5bd0b9f10b393f2320c8e
Tornike <tokobitsadze01@gmail.com>
2025-05-10 12:30:56 +0400

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './partials/header/header.component';
+import { AboutMeComponent } from './partials/about-me/about-me.component';
+import { ProjectsComponent } from './partials/projects/projects.component';
+import { EducationComponent } from './partials/education/education.component';
+import { ExperienceComponent } from './partials/experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, AboutMeComponent, EducationComponent, ProjectsComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
