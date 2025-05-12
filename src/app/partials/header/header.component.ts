ea72709d83a78982f5938988efe3f4c744e6a559
moqalaqe <tokobitsadze01@gmail.com>
2025-05-12 21:37:49 +0400

    header finished

--- src/app/partials/header/header.component.ts
+++ src/app/partials/header/header.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { NgxParticlesModule } from '@tsparticles/angular';
+import { Engine, Container } from '@tsparticles/engine';
+import { loadFull } from 'tsparticles';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgxParticlesModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  id = "tsparticles";
 
+  particlesOptions = {
+    background: {
+      color: {
+        value: "#000000",
+      },
+    },
+    fpsLimit: 60,
+    particles: {
+      number: {
+        value: 200,
+        density: {
+          enable: true,
+          area: 300,
+        },
+      },
+      size: {
+         value: 1
+      },
+      color: {
+        value: "#ffffff",
+      },
+      shape: {
+        type: "circle",
+      },
+      move: {
+        enable: true,
+        speed: 2,
+      },
+    },
+    fullScreen: {
+      enable: true
+    }
+  };
+
+  async particlesInit(engine: Engine): Promise<void> {
+    await loadFull(engine);
+  }
+
+  particlesLoaded(container: Container): void {
+    console.log('Particles loaded', container);
+  }
 }
