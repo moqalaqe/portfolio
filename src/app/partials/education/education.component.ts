4499389d0fb67b03463e33d784acdae9cf8be286
moqalaqe <tokobitsadze01@gmail.com>
2025-05-13 19:15:42 +0400

    mob finished

--- src/app/partials/education/education.component.ts
+++ src/app/partials/education/education.component.ts
@@ -1,12 +1,33 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './education.component.html',
   styleUrl: './education.component.scss'
 })
 export class EducationComponent {
+  backgroundStyle = '';
 
+  onMouseMove(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const rect = target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    this.backgroundStyle = `
+      radial-gradient(
+        circle at ${x}px ${y}px,
+        rgb(0, 123, 255),
+        rgba(17, 58, 220, 0.73) 50%,
+        transparent 80%
+      )
+    `;
+  }
+
+  clearGlow() {
+    this.backgroundStyle = '';
+  }
 }
